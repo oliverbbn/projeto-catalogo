634a36f79dbb274b9e516fcc54f6be5c41d43312
Bruno Bernardo <bbengcivil@gmail.com>
2026-05-06 21:16:00 -0300

    Etapa 04

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $filme = [
     "id" => 1,
-    "titulo" => "Oppenheimer",
+    "titulo" =>"Oppenheimer",    
     "diretor" => "Cristopher Nolan",
     "ano_lancamento" => 2023,
     "nota_imdb" => 8.4,
@@ -13,3 +13,9 @@ echo "Diretor: ".$filme["diretor"]."<br>";
 echo "Ano: ".$filme["ano_lancamento"]."<br>";
 echo "Nota: ".$filme["nota_imdb"]."<br>";
 echo "Genero: ".$filme["genero"]."<br>";
+echo "<br>";
+
+if (isset($filme["diretor"],$filme["nota_imdb"])){
+    echo $filme["diretor"]."<br>";
+    echo $filme["nota_imdb"]."<br>";
+}
